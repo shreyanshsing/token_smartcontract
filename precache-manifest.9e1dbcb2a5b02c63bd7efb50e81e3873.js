e31e9c9c1722030fee6add358ece457c01115011
shreyanshsingh <shreyanshsinghjee@gmail.com>
2020-09-07 22:34:08 +0530

    Updates

---
+++ precache-manifest.9e1dbcb2a5b02c63bd7efb50e81e3873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8da584d721557a79c57348b67496400",
+    "url": "/token_smartcontract/index.html"
+  },
+  {
+    "revision": "24bf12698902a1da15fd",
+    "url": "/token_smartcontract/static/css/main.8376aca7.chunk.css"
+  },
+  {
+    "revision": "5491fed6c64d78db259b",
+    "url": "/token_smartcontract/static/js/2.06668139.chunk.js"
+  },
+  {
+    "revision": "edd670603da73228356d9565dc05648e",
+    "url": "/token_smartcontract/static/js/2.06668139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24bf12698902a1da15fd",
+    "url": "/token_smartcontract/static/js/main.75c1e0c7.chunk.js"
+  },
+  {
+    "revision": "20a7661e1bae62e450a4",
+    "url": "/token_smartcontract/static/js/runtime-main.d6e03ffe.js"
+  }
+]);
